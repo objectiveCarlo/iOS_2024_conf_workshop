4c0125efb2ed0afda04eb58f549c52488d3ce9de
objectiveCarlo <2369416+objectiveCarlo@users.noreply.github.com>
2024-01-18 09:54:51 +0800

    Initial Commit

---
+++ GustoIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GustoIOS
+//
+//  Created by Carlo Luis Martinez on 18/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GustoIOS/GustoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoIOSApp.swift
+//  GustoIOS
+//
+//  Created by Carlo Luis Martinez on 18/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GustoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
